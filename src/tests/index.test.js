b9fb74601a03ea0d45656881f109a6834294419a
Fasasi Toheeb <tohebayomide11@gmail.com>
2022-03-02 07:44:47 +0100

    feat: add more test cases

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -4,20 +4,32 @@ const app = require('../../server')
 describe('Encode endpoint', () => {
   it('Should encode the URL given', async () => {
     const res = await request(app).post('/api/v1/shortner/encode').send({url: 'https://indicina.com'})
+    
     expect(res.statusCode).toEqual(200)
+    expect(res._body.message).toBe('Encode URL successfully')
+    expect(res._body.success).toBe(true)
+    expect(res._body.data).toBeDefined()
   })
 })
 
 describe('Decode endpoint', () => {
   it('Should decode the URL given, to it is original url', async () => {
     const res = await request(app).post('/api/v1/shortner/decode').send({id: 'A'})
+    
     expect(res.statusCode).toEqual(200)
+    expect(res._body.message).toBe('Decode URL successfully')
+    expect(res._body.success).toBe(true)
+    expect(res._body.data).toBeDefined()
   })
 })
 
 describe('Statistics endpoint', () => {
   it('Should get statistic about the given url path', async () => {
-    const res = await request(app).get('/api/v1/shortner/statistic/hello')
+    const res = await request(app).get('/api/v1/shortner/statistic/A')
+    
     expect(res.statusCode).toEqual(200)
+    expect(res._body.message).toBe('Statistics retrieved successfully')
+    expect(res._body.success).toBe(true)
+    expect(res._body.data).toBeDefined()
   })
 })
